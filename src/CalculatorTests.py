759012fbc969d5cac23f98256e72034a20e0206b
Kimberly McDaniel <km438@njit.edu>
2020-03-03 21:26:47 +0000

    Feature: Setup calculator class and test

---
+++ src/CalculatorTests.py
@@ -0,0 +1,8 @@
+import unittest
+from Calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator();
+        self.assertIsInstance(calculator, Calculator);
