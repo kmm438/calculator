f13cd312b141ce458ef9f6207b67a47a09473fe4
Kimberly McDaniel <km438@njit.edu>
2020-03-04 18:57:36 +0000

    Task: Got data to print from Unit Test Addtion.csv

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,5 +1,7 @@
 import unittest
 import csv
+from CsvReader import CsvReader, ClassFactory
+from pprint import pprint
 from Calculator import Calculator
 
 
@@ -11,16 +13,21 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator);
 
-    def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result, 4)
+    def test_addition(self):
+        self.csv_reader = CsvReader('/src/Unit Test Addition.csv').data
 
-    def test_add_method_calculator(self):
-        self.assertEqual(self.calculator.add(2, 2), 4)
-        self.assertEqual(self.calculator.result, 4)
-
-    def test_subtraction_method_calculator(self):
-        self.assertEqual(self.calculator.subtract(2, 2), 0)
-        self.assertEqual(self.calculator.result, 0)
+        for theRow in self.csv_reader:
+            pprint(theRow)
+    # def test_results_property_calculator(self):
+    #     self.assertEqual(self.calculator.result, 4)
+    #
+    # def test_add_method_calculator(self):
+    #     self.assertEqual(self.calculator.add(2, 2), 4)
+    #     self.assertEqual(self.calculator.result, 4)
+    #
+    # def test_subtraction_method_calculator(self):
+    #     self.assertEqual(self.calculator.subtract(2, 2), 0)
+    #     self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
